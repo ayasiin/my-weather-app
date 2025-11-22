a8a3284a3fe458ce7eb3ec718c626aa9e3f356a5
Abubakar.Yasin <ayasiin@hotmail.com>
2025-11-22 15:24:35 +0000

    small js addad

--- weather.js
+++ weather.js
@@ -3,4 +3,12 @@
 const cityInput = document.getElementById('city-input');
 const searchButton = document.getElementById('search-button');
 
-//
+// Select the display elements
+const messageArea = document.getElementById('message-area');
+const weatherDisplay = document.getElementById('weather-display');
+const cityName = document.getElementById('city-name');
+const tempMain= document.getElementById('temperature-main')
+const description = document.getElementById('weather-description')
+const humidityValue = document.getElementById('humidity-value')
+const tempMaxValue = document.getElementById('temp-max-value')
+const tempMinValue = document.getElementById('temp-min-value')
